4c192a91923fc50ed2ec8c83ff35e70ddea7b519
taphacoobams <moustaphasamb719@gmail.com>
2025-05-07 23:21:07 +0200

    second commit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,9 +4,9 @@ export default function Profile() {
       <div className="p-6 space-y-4">
         <h1 className="text-2xl font-bold">Mon profil</h1>
         <div className="space-y-2 text-gray-700">
-          <div><strong>Nom :</strong> Moustapha Sambe</div>
-          <div><strong>Email :</strong> moustapha@example.com</div>
-          <div><strong>Téléphone :</strong> +225 07 00 00 00 00</div>
+          <div><strong>Nom :</strong> Demba Koné</div>
+          <div><strong>Email :</strong> demba@gmail.com</div>
+          <div><strong>Téléphone :</strong> +221 77 000 00 00</div>
           <div><strong>Date de naissance :</strong> 01/01/1990</div>
         </div>
       </div>
